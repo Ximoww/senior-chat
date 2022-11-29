8bf8b4a7e62f638fb4d00f9758d6f03e85387c5a
Ximoww <thibault.rambure@gmail.com>
2022-11-29 20:31:29 +0100

    First Commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/navigation.css";
+
+const Navigation = () => {
+  return (
+    <div className="navigation">
+      <div className="profile"></div>
+    </div>
+  );
+};
+
+export default Navigation;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import "../styles/navigation.css";
+
+const Home = () => {
+  return (
+    <div className="navigation">
+      <Navigation />
+    </div>
+  );
+};
+
+export default Home;
